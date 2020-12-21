e016aea22ab7c11e3b48f851ae556d31b00a9c95
Kanisk <57884127+chakrakan@users.noreply.github.com>
2020-12-20 23:56:35 -0500

    add favicon + gatsby manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,22 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Chakrakan's Portfolio",
+        short_name: 'KC Portfolio',
+        start_url: '/',
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: 'standalone',
+        icon: `${__dirname}/static/images/icons/favicon.png`, // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
   ],
 }
